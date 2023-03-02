955d31d9a31fb01b0fefd69e7b80ceaf377dccbd
Tiago Neves <tiagorockman@gmail.com>
2023-03-02 06:21:47 -0300

    Atualizacao Tabela e Cálculo INSS 2023

--- src/helpers/salary.js
+++ src/helpers/salary.js
@@ -1,36 +1,41 @@
 // Fonte: https://www.todacarreira.com/calculo-salario-liquido/
+// INSS 2023: https://www.contabilizei.com.br/contabilidade-online/desconto-inss/?utm_device=c&utm_term=&utm_source=google&utm_medium=cpc&utm_campaign=%5BMAX%5D_Performance_RNTE&hsa_cam=19671620383&hsa_grp=&hsa_mt=&hsa_src=x&hsa_ad=&hsa_acc=1466761651&hsa_net=adwords&hsa_kw=&hsa_tgt=&hsa_ver=3&gclid=CjwKCAiAr4GgBhBFEiwAgwORrTUa5-vj3VGOpWXE47aTHgeylUSclqidWKrfj_bLtOtM_vG04GvJWBoCOz4QAvD_BwE
+// Confirmando INSS: https://www.coalize.com.br/calculadora-de-inss-resultado
 
 const INSS_TABLE = [
   {
     id: 1,
     minValue: 0,
-    maxValue: 1045,
-    difference: 1045 - 0,
+    maxValue: 1302,
+    difference: 1302 - 0,
     discountPercentage: 0.075,
     discountValue: -1,
   },
   {
     id: 2,
-    minValue: 1045.01,
-    maxValue: 2089.6,
-    difference: 2089.6 - 1045,
+    minValue: 1302.01,
+    maxValue: 2571.29,
+    difference: 2571.29 - 1302,
     discountPercentage: 0.09,
   },
   {
     id: 3,
-    minValue: 2089.61,
-    maxValue: 3134.4,
-    difference: 3134.4 - 2089.6,
+    minValue: 2571.3,
+    maxValue: 3856.94,
+    difference: 3856.94 - 2571.29,
     discountPercentage: 0.12,
   },
   {
     id: 4,
-    minValue: 3134.41,
-    maxValue: 6101.06,
-    difference: 6101.06 - 3134.4,
+    minValue: 3856.95,
+    maxValue: 7507.49,
+    difference: 7507.49 - 3856.94,
     discountPercentage: 0.14,
   },
 ];
+
+//maxSalaryInssTable
+const maxSalaryInssTable = 7507.5;
 
 function round(value) {
   return +value.toFixed(2);
@@ -38,10 +43,7 @@ function round(value) {
 
 function calculateDiscountINSS(baseINSS) {
   let discountINSS = 0;
-
-  if (baseINSS > 6101.07) {
-    return 713.1;
-  }
+ baseINSS = baseINSS >= maxSalaryInssTable ? maxSalaryInssTable : baseINSS;
 
   for (var i = 0; i < INSS_TABLE.length; i++) {
     var currentItem = INSS_TABLE[i];
